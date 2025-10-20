43df9841e76dbdeaedeb6f5272cb08afbf70f4f1
DarkKnight8247 <franz.castor25@gmail.com>
2025-10-20 23:32:20 +0800

    Socket Connection Using PHP

--- Midterm/Act 3/client.php
+++ Midterm/Act 3/client.php
@@ -3,7 +3,7 @@ $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
 $hostIP = '192.168.0.2';
 $port = 12345;
-
+ 
 $connect = socket_connect($socket, $hostIP, $port);
 
 $message = "Hello Server, I am Castor, Franz - the Client";
